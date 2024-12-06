4580fae3373a2ff5037f5625619ed16940a9e498
Francisfer <114183517+Francisfer@users.noreply.github.com>
2024-12-06 18:11:25 +0000

    Started javascript, finished data types and variables, started operations in javascript.

---
+++ js/dataTypes_variabes_constants.js
@@ -0,0 +1,44 @@
+console.log("hello");
+console.log("hi");
+
+// Both print, each have it's own purpose, not for now.
+// process.stdout.write("Francis");
+
+console.table({
+  city: "Worlds end.",
+  name: "Nobody",
+});
+
+// Makes more sense further ahead.
+console.warn("Nop");
+
+// Number
+let score = 100;
+
+// String
+let name = "Francis";
+
+// Boolean
+let isLoggedIn = false;
+
+// Object (arrays, objects)
+
+// array / collection
+letTeaTypesArray = ["lemon tea", "orange tea", "oolong tea"];
+
+// object
+let user = {
+  firsName: "Francis",
+  lastName: "Ferreira",
+};
+
+////////////////////////////////////////////////////
+
+let game = "spiderman";
+
+game = "batman";
+
+// This results in the error assignment to constant variable.
+const userName = "Francis";
+
+userName = "Joe";
