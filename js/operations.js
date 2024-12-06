f416df4b473c17d66d7e2a6c002ec35b06fcdfcc
Francisfer <114183517+Francisfer@users.noreply.github.com>
2024-12-06 19:28:58 +0000

    Finished operations in javascript.

---
+++ js/operations.js
@@ -0,0 +1,68 @@
+// 1
+let addition = 4 + 4;
+
+// 2
+let subtraction = 5 - 2;
+
+// 3
+let multiplication = 5 * 5;
+
+// 4
+let dividion = 10 / 2;
+
+// 5
+let remainder = 7 % 3;
+
+// console.log(remainder);
+
+// 6
+let exponent = 2 ** 3;
+
+// 7
+let increment = 19;
+increment++;
+// console.log(increment);
+
+// 8
+let decrement = 19;
+decrement--;
+// console.log(decrement);
+
+// 9
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 5;
+
+///////////////////////////////////////////
+
+// Comparison operations.
+
+console.log(num1 == num2); // true
+console.log(num1 != num3); // true
+console.log(num2 > num3); // false
+console.log(num2 < num3); // true
+
+///////////////////////////////////////////
+
+// Logical operations.
+
+let isLoggedIn = true;
+let isPaid = false;
+
+console.log(isLoggedIn && isPaid);
+console.log(isLoggedIn || isPaid);
+
+///////////////////////////////////////////
+
+// Assignment operations.
+
+let score = 10 + 10;
+
+let number = 10;
+
+number += 5;
+
+score -= 5;
+
+console.log(score, number);
