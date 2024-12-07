38dfdd9734492e75af1d9f53c44df5e92b13089b
Francisfer <114183517+Francisfer@users.noreply.github.com>
2024-12-07 09:58:59 +0000

    Finished primitives, started non-primitives.

--- js/02-part2/primitives.js
+++ js/02-part2/primitives.js
@@ -1,4 +1,4 @@
-// Number
+// NUMBER
 
 // Type of - To know the type of data
 
@@ -13,3 +13,43 @@ let newBalance = new Number(100);
 console.log(newBalance);
 
 console.log(newBalance.valueOf());
+
+console.log(typeof newBalance);
+
+// This also works with booleans.
+
+let isActive = true;
+let isReallyActive = new Boolean(true);
+
+// STRING
+
+let myString = "Hola";
+let userName = "Francis";
+let anotherString = "Bilbo";
+
+// Old way
+
+let oldGreet = myString + " " + "Jonas";
+console.log(oldGreet);
+
+// New way - String concatenation.
+
+let newGreet = `Hello ${userName}`;
+console.log(newGreet);
+
+// String concatenation allows computations.
+
+let something = `The value is ${2 + 2}`;
+
+// SYMBOL
+
+let symbol1 = Symbol();
+let symbol2 = Symbol();
+
+// Although the appear to have the same value, if we test it, we see that their are not equal.
+
+console.log(symbol1 == symbol2); // False
+
+// This happens although they have the same value.
+
+console.log(symbol1, symbol2); // Symbol() Symbol()
