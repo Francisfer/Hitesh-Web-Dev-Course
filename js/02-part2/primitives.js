c4b657448967ac22adc7ce9f38caf03dd0538b15
Francisfer <114183517+Francisfer@users.noreply.github.com>
2024-12-07 09:07:33 +0000

    Started primitive data methods.

---
+++ js/02-part2/primitives.js
@@ -0,0 +1,15 @@
+// Number
+
+// Type of - To know the type of data
+
+let balance = 129;
+
+console.log(typeof balance);
+
+// The new keyword, used for classes, generates an array, when generating a new number for ex.
+
+let newBalance = new Number(100);
+
+console.log(newBalance);
+
+console.log(newBalance.valueOf());
